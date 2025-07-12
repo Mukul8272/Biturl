d210324a496eca10bfae56ce4095ac96ed082462
MUKUL <mukultomar671@gmail.com>
2025-07-12 22:39:12 +0530

    bug fix

--- app/api/shorten/route.js
+++ app/api/shorten/route.js
@@ -1,27 +1,4 @@
-// import clientPromise from "@/lib/mongodb"
 
-// export async function POST(request) {
-
-//     const body=await request.json()
-//     console.log(body)
-
-
-//     const client= await clientPromise;
-//     const db=client.db("biturl")
-//     const collection = db.collection("url")
-
-//     const doc = await collection.findOne({shorturl: body.shorturl})
-//     if(doc){
-//         return Response.json({success:false, error:true, message: "URL already exist"})
-//     }
-
-//     const result = await collection.insertOne({
-//         url:body.url,
-//         shorturl:body.shorturl
-//     })
-
-//     return Response.json({success: true, error: false, message: "URL generated successfully"})
-// }
 export const runtime = "nodejs";
 
 import clientPromise from "@/lib/mongodb";
